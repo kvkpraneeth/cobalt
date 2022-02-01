58c82fa5620358161f2649a5c24af039d54eebe7
Harshavardhan341 <harshavardhan.kulkarni@learner.manipal.edu>
2022-02-01 19:36:57 +0530

    added exp_r3_sim

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 # Cheap Method.
 
-source /opt/ros/foxy/setup.bash
+ROS2 #alias for source /opt/ros/foxy/setup.bash
 colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --packages-select cobalt_utils --allow-overriding cobalt_utils
-source ./install/setup.bash
+srcit #alias for source ./install/setup.bash
 colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --packages-select x3_sim --allow-overriding x3_sim
-source ./install/setup.bash
+srcit
 colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --packages-select husky_sim --allow-overriding husky_sim
-source ./install/setup.bash
+srcit
+colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --packages-select exp_r3_sim --allow-overriding exp_r3_sim
+srcit
